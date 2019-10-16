712b870daad3537f5463a1cad0ca09e8984f29de
LowTrunkOzz <56564823+LowTrunkOzz@users.noreply.github.com>
2019-10-16 19:30:52 -0400

    Add files via upload

--- config.h
+++ config.h
@@ -1,5 +1,12 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                                 //
+//Be sure to update these parameters accordingly. "YOUR_PIN" and "YOUR_NAME Smart Garage Door" MUST be in          //
+//in parenthesis for the code to execute properly. If you want the web interface available globally, set port      //
+//forwarding rules from your router's settings accordingly. I usually use 5142 for the port but you can set it to  //
+//whatever you'd like as long as it isn't a commonly used port. "YOUR_PIN" should be any easily remembered four    //
+//digit string and is required for operation. I really wouldn't suggest making 0000 or any easily guessed string.  //
+//                                                                                                                 //
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 int serverPort = YOUR_PORT;
 const char* (pass_sent) = "YOUR_PIN";
 #define h1_ID "YOUR_NAME Smart Garage Door";
-
-
